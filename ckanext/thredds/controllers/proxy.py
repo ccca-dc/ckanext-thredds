5ffd4fde7b9fc96af5635ac09f7a894ddc7b0eab
Georg Seyerl <gseyerl@zaalpc33.zamg.ac.at>
2017-03-02 16:20:20 +0100

    Controller with X Sendfile from uwsgi to nginx

--- ckanext/thredds/controllers/proxy.py
+++ ckanext/thredds/controllers/proxy.py
@@ -22,36 +22,24 @@ log = logging.getLogger(__name__)
 
 
 class WMSProxyController(base.BaseController):
-    def wms_proxy(self):
+    def wms_proxy(self, resource_id):
+        """
+        Provides a direct download by either redirecting the user to the url
+        stored or downloading an uploaded file directly.
+        """
         context = {'model': model, 'session': model.Session,
-                   'user': c.user}
-        #toolkit.check_access('resource_update',
-        thredds_server = "10.1.186.222:8080"
-        p_url = urlparse(request.params.get('url'))
-        p_url = p_url._replace(netloc=thredds_server)
-        r = requests.get(p_url.geturl())
-        log.debug(p_url.geturl())
-        #r = requests.get(url)
-#        if url.find("GetCapabilities") != -1:
-#            log.debug(url)
-#            log.debug(r.headers)
-        return r.content
+                   'user': c.user, 'auth_user_obj': c.userobj}
 
-#$url = $_GET['url'];
-#$result = "";
-#if (strpos($url, "GetCapabilities") >= 0){
-#    $result = file_get_contents($url);
-#    $nlines = count($http_response_header);
-#    for ($i = $nlines-1; $i >= 0; $i--) {
-#        $line = $http_response_header[$i];
-#        if (substr_compare($line, 'Content-Type', 0, 12, true) == 0) {
-#            $content_type = $line;
-#            break;
-#        }
-#    }
-#    header($content_type);
-#    echo $result;
-#} else{
-#    header('HTTP/1.0 400 Bad Request');
-#    echo 'Request not valid';
-#}
+        try:
+            rsc = get_action('resource_show')(context, {'id': resource_id})
+        except (NotFound, NotAuthorized):
+            abort(404, _('Resource not found'))
+
+        if authz.auth_is_anon_user(context):
+            abort(401, _('Unauthorized to read resource %s') % resource_id)
+        else:
+            if rsc.get('url_type') == 'upload':
+                p_url = urlparse(request.params.get('url'))
+                response.headers['X-Accel-Redirect'] = "{0}".format(p_url.path)
+                return response
+            abort(404, _('No wms available'))
