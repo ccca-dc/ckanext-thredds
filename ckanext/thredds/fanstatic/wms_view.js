d5dbea3f3a48179a9f68ddca7e8c6efee1c1da63
Georg Seyerl <gseyerl@zaalpc33.zamg.ac.at>
2017-03-29 16:43:45 +0200

    Restructure wms_view Add function showPreview

--- ckanext/thredds/fanstatic/wms_view.js
+++ ckanext/thredds/fanstatic/wms_view.js
@@ -9,8 +9,22 @@ ckan.module('wms_view', function ($) {
         $.proxyAll(this, /_on/);
         var options = this.options;
 
+        this.sandbox = ckan.sandbox();
+
+        this.sandbox.client.call('GET','thredds_get_layers',
+                                '?id='+ this.options.resource_id,
+                                this._onHandleData,
+                                this._onHandleData
+                                );
+
         var startDate = new Date();
         startDate.setUTCHours(0, 0, 0, 0);
+
+      },
+
+      showPreview: function (wmtsInfo) {
+
+        var layers = $.map(wmtsInfo, function( value, key ) { return key.toString(); } );
 
         var map = L.map('map', {
             zoom: 7,
@@ -26,18 +40,11 @@ ckan.module('wms_view', function ($) {
             center: [47.3, 13.9]
         });
 
-        this.sandbox = ckan.sandbox();
-        // ckan.sandbox('GET','thredds_get_layers','?id=decaacbb-3979-4b14-9b7a-abaee64bc983', function(json) {console.log(json)},function(json) {console.log(json)});
-        this.sandbox.client.call('GET','thredds_get_layers',
-                                 '?id='+ this.options.resource_id,
-                                 this._onHandleData,
-                                 function(json) {console.log(json);}
-                                ),
 
         var cccaWMS = "https://sandboxdc.ccca.ac.at/wms_proxy/" + this.options.resource_id;
 
         var cccaHeightLayer = L.tileLayer.wms(cccaWMS, {
-            layers: window.wmslayer,
+            layers: layers,
             format: 'image/png',
             transparent: true,
             colorscalerange: '-20,20',
@@ -105,16 +112,17 @@ ckan.module('wms_view', function ($) {
 
         cccaHeightTimeLayer.addTo(map);
 
-    },
+      },
 
     _onHandleData: function(json) {
         if (json.success) {
-            $.map(json.result, function( value, key ) {
-                window.wmslayer = key.toString(); }
-                 );
-
+            self.showPreview(json.result)
         }
-    }
+    },
+
+		_onHandleError: function(error) {
+        console.log(error);
+		}
 
   };
 });
