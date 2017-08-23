41064e96274e690bc2a2ac1a828ef1d4752f3789
KatharinaSack <katharina.sack@ccca.ac.at>
2017-08-23 15:34:07 +0200

    point and bbox now works

--- ckanext/thredds/fanstatic/stored_queries.js
+++ ckanext/thredds/fanstatic/stored_queries.js
@@ -3,7 +3,24 @@ $('ul#querylist li').click( function() {
     for (i = 0; i < params.length; i++) {
         if (params[i].match(/[a-z]/i)){
             split_param = $.trim(params[i]).split(': ')
-            if($("[name='" + split_param[0] + "']").is(':radio') != true){
+            if(split_param[0] == "north"){
+                $('#southWest').show();
+                $('label[for="north"]').text("North");
+                $('label[for="east"]').text("East");
+
+                $('#north').val(split_param[1]);
+            } else if(split_param[0] == "latitude"){
+                $('#south').val("");
+                $('#west').val("");
+                $('#southWest').hide();
+                $('label[for="north"]').text("Latitude");
+                $('label[for="east"]').text("Longitude");
+
+                $('#north').val(split_param[1]);
+            } else if(split_param[0] == "longitude"){
+                $('#east').val(split_param[1]);
+            }
+            else if($("[name='" + split_param[0] + "']").is(':radio') != true){
                 $("[name='" + split_param[0] + "']").val(split_param[1]);
             }else{
                 for (j = 0; j < $("[name='" + split_param[0] + "']").length; j++) {
